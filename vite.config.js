4567332b9f4f5346bac4e46bea20771ee9ae520a
intrnl <intrnl28+git@gmail.com>
2020-06-22 09:25:07 +0700

    add these to optimizedeps

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,13 @@ let config = {
   plugins: [
     svelte(),
   ],
+
+  optimizeDeps: {
+    include: [
+      'vite-plugin-svelte/runtime/api',
+      'vite-plugin-svelte/runtime/adapter',
+    ],
+  },
 };
 
 export default config;
